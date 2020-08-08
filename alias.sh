1c99105922638a3a29ff6cbb610fb04ec4058d76
Sida Chen <git@sidac.me>
2020-08-08 19:53:39 -0400

    Init Guitar Tuner

--- alias.sh
+++
@@ -1,5 +0,0 @@
-# always mount into local drive
-# always edit locally and run in remote
-mt='`gcloud alpha cloud-shell get-mount-command ~/cloud`'
-kk='gcloud alpha cloud-shell ssh'
-# then we can code
--- code-server.sh
+++
@@ -1,1 +0,0 @@
-docker run -t -p 127.0.0.1:8443:8443 -v "${PWD}:/root/project" quay.io/keyboardnerd/dev:code-server-latest code-server --allow-http --no-auth
